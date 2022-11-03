e746550459aae77528f8e75ffe53591b0fd691e1
Chiến Trần <116490257+tcc3281@users.noreply.github.com>
2022-11-03 20:34:42 +0700

    Add files via upload

---
+++ shopnhalam/js/slider.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", function(){
+    const slider = document.querySelector(".slider");
+    const sliderMain = document.querySelector(".slider-main");
+    const sliderItems = document.querySelectorAll(".slider-item");
+    const nextBtn = document.querySelector(".slider-next");
+    const prevBtn = document.querySelector(".slider-prev");
+    const dotItem = document.querySelectorAll(".slider-dot-item");
+    const sliderItemWidth = sliderItems[0].offsetWidth;
+    const slidesLength = sliderItems.length;
+    let postionX = 0;
+    let index = 0;
+    nextBtn.addEventListener("click", function(){
+        handleChangeSlide(1);
+    });
+
+    prevBtn.addEventListener("click", function(){
+        handleChangeSlide(-1);
+    });
+    [...dotItem].forEach(item => 
+        item.addEventListener("click", function(e) {
+            [...dotItem].forEach(el => el.classList.remove("active")); 
+            e.target.classList.add("active"); 
+            const slideIndex = parseInt(e.target.dataset.index);
+            index = slideIndex;
+            postionX = -1 * index * sliderItemWidth;
+            sliderMain.style = `transform: translateX(${postionX}px)`;
+        })
+    );
+    function handleChangeSlide(direction){
+        if(direction===1){              
+            if(index == slidesLength-1) {
+                postionX = sliderItemWidth; index=-1;
+            }
+            postionX = postionX - sliderItemWidth;
+            sliderMain.style = `transform: translateX(${postionX}px)`;
+            index++;
+        }
+        else if(direction===-1){
+                
+            if(index == 0) {
+                index = slidesLength; postionX = -1 * sliderItemWidth * slidesLength; 
+            }
+            postionX = postionX + sliderItemWidth;
+            sliderMain.style = `transform: translateX(${postionX}px)`;
+            index--; 
+        }
+        [...dotItem].forEach((el) => el.classList.remove("active"));
+        dotItem[index].classList.add("active");
+    }
+    setInterval(function(){
+        handleChangeSlide(1);
+    }, 3000);
+})
